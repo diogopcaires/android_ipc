42d24855fd5d2a4ec16d942b6f8ebc60ecd3a1e8
diogosilva <diogo.pcaires@gmail.com>
2018-09-05 19:28:21 -0300

    create a repository property

--- client-app/src/main/java/com/diogopires/client_app/ui/MainActivity.kt
+++ client-app/src/main/java/com/diogopires/client_app/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.diogopires.client_app.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.diogopires.client_app.R
+import com.diogopires.client_app.data.Repository
 import com.diogopires.client_app.data.RepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -11,14 +12,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var repository: Repository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sender_btn.setOnClickListener({
-            val api = RepositoryImpl(applicationContext)
+            repository = RepositoryImpl(applicationContext)
 
-            api.getCharacter()
+            repository.getCharacter()
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribe({ value ->
